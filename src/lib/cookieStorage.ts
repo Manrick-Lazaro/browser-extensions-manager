33539f7ea86cc7dcb269207637674ac73984e16d
Manrick-Lazaro <manricksantos@gmail.com>
2025-06-10 15:28:11 -0300

    feat: create cookie storage

---
+++ src/lib/cookieStorage.ts
@@ -0,0 +1,21 @@
+// lib/cookie-storage.ts
+import Cookies from "js-cookie";
+
+export const cookieStorage = {
+  getItem: (name: string) => {
+    if (typeof window !== "undefined") {
+      return Cookies.get(name) || "light";
+    }
+    return "light";
+  },
+  setItem: (name: string, value: string) => {
+    Cookies.set(name, value, {
+      expires: 30, // 1 ano
+      sameSite: "lax",
+      path: "/",
+    });
+  },
+  removeItem: (name: string) => {
+    Cookies.remove(name);
+  },
+};
