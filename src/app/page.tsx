37b1fec2a2dac26f1f88a54d0ec371c0f71c0888
Manrick-Lazaro <manricksantos@gmail.com>
2025-06-11 13:39:43 -0300

    feat: changes the way retrieve data.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,21 @@
 import { JSX, useEffect, useState } from "react";
 
 import Header from "../components/Header";
-import Card, { CardProps } from "../components/Card";
+import Card from "../components/Card";
+import { ExtensionType } from "../types/extension";
 
 export default function Home(): JSX.Element {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<ExtensionType[]>([]);
+
+  async function getItems() {
+    const res = await fetch("/api/json-handler");
+    const data = await res.json();
+    setData(data);
+  }
 
   useEffect(() => {
-    fetch("/api/json-handler")
-      .then((res) => res.json())
-      .then((json) => {
-        setData(json);
-      });
+    getItems();
   }, []);
-
-  // function editJsonFile() {
-  //   const jsonString = JSON.stringify(data);
-  // }
 
   return (
     <div className="bg-linear-to-b from-light-gradient-from to-light-gradient-to dark:from-dark-gradient-from dark:to-dark-gradient-to min-h-lvh w-full">
@@ -26,13 +25,15 @@ export default function Home(): JSX.Element {
         <Header />
 
         <div className="mt-6 gap-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-          {data.map((item: CardProps) => (
+          {data.map((item: ExtensionType) => (
             <Card
+              id={item.id}
               key={item.name}
               name={item.name}
               logo={item.logo}
               description={item.description}
               isActive={item.isActive}
+              syncData={() => getItems()}
             />
           ))}
         </div>
