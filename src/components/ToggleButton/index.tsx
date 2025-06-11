aa051f5d9ff33817504bfae2669ee3d66d8ebf28
Manrick-Lazaro <manricksantos@gmail.com>
2025-06-11 13:41:29 -0300

    fix: adjusment in directon toggle and color.

--- src/components/ToggleButton/index.tsx
+++ src/components/ToggleButton/index.tsx
@@ -1,21 +1,34 @@
 import { useState } from "react";
 
-export default function ToggleButton() {
-  const [toggle, setToggle] = useState(false);
+type toggleButtonProps = {
+  isActive: boolean;
+  onChange: () => void;
+};
+
+export default function ToggleButton({
+  isActive,
+  onChange,
+}: toggleButtonProps) {
+  const [toggle, setToggle] = useState(isActive);
+
+  function handleToggle() {
+    onChange();
+    setToggle(!toggle);
+  }
 
   return (
     <div className="flex items-center justify-center h-full">
       <button
         className={`w-14 h-7 rounded-full cursor-pointer shadow-lg transition-colors duration-200 relative flex items-center ${
           toggle
-            ? "bg-neutral-300 dark:bg-neutral-600"
-            : "bg-red-700 dark:bg-red-400"
+            ? "bg-red-700 dark:bg-red-400"
+            : "bg-neutral-300 dark:bg-neutral-600"
         }`}
-        onClick={() => setToggle(!toggle)}
+        onClick={() => handleToggle()}
       >
         <div
           className={`h-5 w-5 bg-neutral-0 absolute rounded-full transform transition-transform duration-300 ${
-            toggle ? "translate-x-1" : "translate-x-7.5"
+            toggle ? "translate-x-7.5" : "translate-x-1"
           }`}
         ></div>
       </button>
