b71fd2dccf8c716782010be62367e741669926ec
Manrick-Lazaro <manricksantos@gmail.com>
2025-06-12 12:22:14 -0300

    feat: create loading component.

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center h-full p-4">
+      <div className="animate-spin rounded-full h-12 w-12 border-4 border-neutral-900 border-t-transparent"></div>
+    </div>
+  );
+}
