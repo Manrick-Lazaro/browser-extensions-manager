bda5550e82686f829e9a7b3c0be0e5e5cbaa31f0
Manrick-Lazaro <manricksantos@gmail.com>
2025-06-12 14:08:55 -0300

    feat: change the directory of the button component

--- src/components/ButtonComponent/ButtonComponent.tsx
+++ src/components/ButtonComponent/ButtonComponent.tsx
@@ -2,18 +2,30 @@ type ButtonProps = {
   text: string;
   activatedFilter?: string;
   onChangeFilter?: (filter: string) => void;
+  w?: number;
+  h?: number;
+  desabledShadow?: boolean;
 };
 
 export default function ButtonComponent({
   activatedFilter,
   onChangeFilter,
   text,
+  w,
+  h,
+  desabledShadow,
 }: ButtonProps) {
   return (
     <button
       className={`common-btn ${
         activatedFilter === text ? "active-btn" : "inactive-btn"
-      }`}
+      } ${desabledShadow ? "!shadow-none" : ""}`}
+      style={{
+        paddingLeft: w ?? 20,
+        paddingRight: w ?? 20,
+        paddingTop: h ?? 10,
+        paddingBottom: h ?? 10,
+      }}
       onClick={() => {
         if (onChangeFilter) {
           onChangeFilter(text);
