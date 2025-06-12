1086f3896f1c015c31d8be5426425055740e5b35
Manrick-Lazaro <manricksantos@gmail.com>
2025-06-12 12:24:24 -0300

    feat: using filter to get the data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,32 @@ import Header from "@/components/Header";
 import Card from "@/components/Card";
 import Filter from "@/components/Filter";
 import { ExtensionType } from "../types/extension";
+import Loading from "@/components/Loading";
 
 export default function Home(): JSX.Element {
   const [data, setData] = useState<ExtensionType[]>([]);
+  const [filter, setFilter] = useState("All");
+  const [loading, setLoading] = useState(false);
 
   async function getItems() {
-    const res = await fetch("/api/json-handler");
+    let apiPath = "/api/json-handler";
+
+    if (filter !== "All") {
+      apiPath = `${apiPath}?filter=${filter}`;
+    }
+
+    const res = await fetch(apiPath);
     const data = await res.json();
+
     setData(data);
   }
 
   useEffect(() => {
-    getItems();
-  }, []);
+    setLoading(true);
+
+    getItems().finally(() => setLoading(false));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [filter]);
 
   return (
     <div className="bg-linear-to-b from-light-gradient-from to-light-gradient-to dark:from-dark-gradient-from dark:to-dark-gradient-to min-h-lvh w-full">
@@ -26,22 +39,27 @@ export default function Home(): JSX.Element {
         <Header />
 
         <div className="mt-9 mb-8">
-          <Filter />
+          <Filter activatedFilter={filter} onChangeFilter={setFilter} />
         </div>
 
-        <div className="mt-6 gap-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-          {data.map((item: ExtensionType) => (
-            <Card
-              id={item.id}
-              key={item.name}
-              name={item.name}
-              logo={item.logo}
-              description={item.description}
-              isActive={item.isActive}
-              syncData={() => getItems()}
-            />
-          ))}
-        </div>
+        {loading ? (
+          <div className="mt-6">
+            <Loading />
+          </div>
+        ) : (
+          <div className="mt-6 gap-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+            {data.map((item: ExtensionType) => (
+              <Card
+                id={item.id}
+                key={item.id}
+                name={item.name}
+                logo={item.logo}
+                description={item.description}
+                isActive={item.isActive}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
