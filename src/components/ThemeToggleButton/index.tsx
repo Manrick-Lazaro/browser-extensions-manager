bd7a44028b9df7aa3fff554f27e8aa728f5e5f53
Manrick-Lazaro <manricksantos@gmail.com>
2025-06-21 14:37:00 -0300

    feat: uncomment icon imports and usage in ThemeToggleButton component

--- src/components/ThemeToggleButton/index.tsx
+++ src/components/ThemeToggleButton/index.tsx
@@ -2,8 +2,8 @@
 
 import { useThemeStore } from "../../hooks/theme";
 
-// import IconMoon from "../../assets/images/icon-moon.svg";
-// import IconSun from "../../assets/images/icon-sun.svg";
+import IconMoon from "../../assets/images/icon-moon.svg";
+import IconSun from "../../assets/images/icon-sun.svg";
 
 export function ThemeToggleButton() {
   const { theme, switchTheme } = useThemeStore();
@@ -12,11 +12,11 @@ export function ThemeToggleButton() {
     <div onClick={() => switchTheme()}>
       {theme === "light" ? (
         <div className="bg-neutral-100 flex-1 justify-center items-center rounded-xl p-3 overflow-auto">
-          {/* <IconMoon alt="logo" width={25} /> */}
+          <IconMoon alt="logo" width={25} />
         </div>
       ) : (
         <div className="bg-neutral-700 flex-1 justify-center items-center rounded-xl p-3 overflow-auto">
-          {/* <IconSun alt="logo" width={25} /> */}
+          <IconSun alt="logo" width={25} />
         </div>
       )}
     </div>
